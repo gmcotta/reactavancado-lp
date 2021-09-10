305fc9ad803304fa4c5248f0cb811cf1ecf12c47
Gustavo Cotta <gmcotta34@gmail.com>
2021-09-09 21:35:29 -0300

    pass props to logo component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,12 +13,14 @@ import SectionReviews from 'components/SectionReviews'
 import SectionFaq from 'components/SectionFaq'
 import Footer from 'components/Footer'
 import JsonSchema from 'components/JsonSchema'
+
 import client from 'graphql/client'
 import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
+import { LandingPageProps } from 'types/api'
 
-const Index = () => (
+const Index = ({ logo }: LandingPageProps) => (
   <>
-    <SectionHero />
+    <SectionHero logo={logo} />
     <SectionAboutProject />
     <SectionTech />
     <SectionConcepts />
