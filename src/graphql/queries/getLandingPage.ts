75979b7d8686132ae3f8f59cee2d3f3914e3a1f4
Gustavo Cotta <gmcotta34@gmail.com>
2021-09-11 15:10:35 -0300

    make header dynamic

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,10 +1,30 @@
 const GET_LANDING_PAGE = /* GraphQL */ `
-  query GET_LANDING_PAGE {
-    landingPage(id: 1) {
-      logo {
+  fragment logoData on LandingPage {
+    logo {
+      alternativeText
+      url
+    }
+  }
+
+  fragment headerData on LandingPage {
+    header {
+      title
+      description
+      media {
         alternativeText
         url
       }
+      button {
+        label
+        url
+      }
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage(id: 1) {
+      ...logoData
+      ...headerData
     }
   }
 `
--- src/types/api.ts
+++ src/types/api.ts
@@ -3,6 +3,20 @@ export type LogoProps = {
   url: string
 }
 
+export type HeaderProps = {
+  title: string
+  description: string
+  media: {
+    alternativeText: string
+    url: string
+  }
+  button: {
+    label: string
+    url: string
+  }
+}
+
 export type LandingPageProps = {
   logo: LogoProps
+  header: HeaderProps
 }
